bf3b1548893c91069f0eb6d61bee7cd0bcf3a8f7
Justin McDaniel <jmcdaniel001@gmail.com>
2013-11-08 21:44:03 -0600

    Made my code super nasty to pass through error messages from Amazon for bulk requests. I'll perhaps investigate a cleaner way in the future.

--- lib/ruby-mws/api/product.rb
+++ lib/ruby-mws/api/product.rb
@@ -14,10 +14,16 @@ module MWS
         :mods => [
           lambda { |r|
             r.each do |lowest_offers|
-              if lowest_offers.product.lowest_offer_listings.respond_to?("lowest_offer_listing")
-                lowest_offers.product.lowest_offer_listings = [lowest_offers.product.lowest_offer_listings.lowest_offer_listing].flatten
+              if (lowest_offers.respond_to?("product")) # I have noticed amazon does not always respond with an error when there is an error, so this is the best check I can do.
+                if lowest_offers.product.lowest_offer_listings.respond_to?("lowest_offer_listing")
+                  lowest_offers.product.lowest_offer_listings = [lowest_offers.product.lowest_offer_listings.lowest_offer_listing].flatten
+                else
+                  lowest_offers.product.lowest_offer_listings = []
+                end
               else
-                lowest_offers.product.lowest_offer_listings = []
+                lowest_offers.product = {lowest_offer_listings:[]}
+                # Set a generic error message if Amazon incorrectly responded as "success".
+                lowest_offers.error.message = "An error occurred." unless lowest_offers.respond_to?("error")
               end
             end
             }
@@ -34,13 +40,25 @@ module MWS
         :mods => [
           lambda { |r|
             r.each do |product|
-              product.products = [product.products.product].flatten
-              product.products.each do |real_product|
-                if real_product.sales_rankings.respond_to?("sales_rank")
-                  real_product.sales_rankings = [real_product.sales_rankings.sales_rank].flatten
+              if (product.respond_to?("products")) # I have noticed amazon does not always respond with an error when there is an error, so this is the best check I can do.
+                # OMG shoot me this code sucks!!! 俺はAMAZON大きらい!!!
+                if product.products.respond_to?("product")
+                  product.products = [product.products.product].flatten 
                 else
-                  real_product.sales_rankings = []
+                  product.products = []
                 end
+
+                product.products.each do |real_product| # This has got to be an insane slowdown... will look into that later.
+                  if real_product.sales_rankings.respond_to?("sales_rank")
+                    real_product.sales_rankings = [real_product.sales_rankings.sales_rank].flatten
+                  else
+                    real_product.sales_rankings = []
+                  end
+                end
+              else
+                product.products = []
+                # Set a generic error message if Amazon incorrectly responded as "success".
+                product.error.message = "An error occurred." unless product.respond_to?("error")
               end
             end
             }
